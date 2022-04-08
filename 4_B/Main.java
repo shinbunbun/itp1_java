3f9490522d4dd53a1941208bbf2ff7616b2b457a
shinbunbun <takamuneyuto@gmail.com>
2022-04-08 19:06:49 +0900

    add 4_B

---
+++ 4_B/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    double r = sc.nextDouble();
+    sc.close();
+    System.out.printf("%f %f\n", r * r * Math.PI, 2 * r * Math.PI);
+  }
+}
