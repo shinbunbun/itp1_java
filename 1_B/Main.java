fd1d7300e64ef91096e450a32d5a674dbe5068d7
shinbunbun <takamuneyuto@gmail.com>
2022-04-08 19:18:12 +0900

    fix 1_B

--- 1_B/Main.java
+++ 1_B/Main.java
@@ -4,6 +4,7 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int x = sc.nextInt();
+    sc.close();
     System.out.println((int) Math.pow(x, 3));
   }
 }
