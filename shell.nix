0b3a1353d8d7342b906a50f912cf0f464d3c9e01
shinbunbun <takamuneyuto@gmail.com>
2022-04-15 14:09:22 +0900

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,24 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.buildPackages.openjdk11];
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  setting = builtins.toJSON {
+    folders = [
+      {
+        path = ".";
+      }
+    ];
+    settings = {
+      "nixEnvSelector.nixFile" = "\$\{workspaceRoot\}/shell.nix";
+      "java.format.settings.url" = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml";
+      "java.format.settings.profile" = "GoogleStyle";
+      "java.jdt.ls.java.home" = "${pkgs.buildPackages.openjdk11.outPath}/lib/openjdk";
+    };
+  };
+in
+pkgs.mkShell
+{
+  nativeBuildInputs = [ pkgs.buildPackages.openjdk11 pkgs.zip ];
+  shellHook = ''
+    echo '${setting}' > ./itp1_java.code-workspace
+  '';
 }
